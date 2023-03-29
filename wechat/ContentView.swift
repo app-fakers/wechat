2a585a9e491b59fc1d59e5ad284f3bdc72819b9f
ns-cn <ns-cn@qq.com>
2023-03-29 22:14:00 +0800

    Initial Commit

---
+++ wechat/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  wechat
+//
+//  Created by 唐毓俊 on 2023/3/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ wechat/wechatApp.swift
@@ -0,0 +1,17 @@
+//
+//  wechatApp.swift
+//  wechat
+//
+//  Created by 唐毓俊 on 2023/3/29.
+//
+
+import SwiftUI
+
+@main
+struct wechatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
