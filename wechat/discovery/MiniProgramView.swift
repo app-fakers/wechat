2400f0dd505a88845f5296f1f7bf11cf586fcbbb
ns-cn <ns-cn@qq.com>
2023-04-05 12:39:07 +0800

    更新

---
+++ wechat/discovery/MiniProgramView.swift
@@ -0,0 +1,20 @@
+//
+//  MiniProgramView.swift
+//  wechat
+//
+//  Created by 唐毓俊 on 2023/4/4.
+//
+
+import SwiftUI
+
+struct MiniProgramView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MiniProgramView_Previews: PreviewProvider {
+    static var previews: some View {
+        MiniProgramView()
+    }
+}
